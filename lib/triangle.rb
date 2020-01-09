9c9face6d76a0ce7c86db59e68b81a0157881dd9
Nicole Rothenay <nthorn1211@gmail.com>
2020-01-09 01:51:51 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,39 +1,30 @@
 class Triangle
-  def initialize(side_1, side_2, side_3)
-    @triangle_sides = []
-    @triangle_sides << side_1
-    @triangle_sides << side_2
-    @triangle_sides << side_3
+  attr_accessor :a, :b, :c
+  def initialize(a, b, c)
+    @a=a
+    @b=b
+    @c=c
   end
 
   def valid?
-    sum_one_two = @triangle_sides[0] + @triangle_sides[1]
-    sum_one_three = @triangle_sides[0] + @triangle_sides[2]
-    sum_two_three = @triangle_sides[1] + @triangle_sides[2]
-
-    if (@triangle_sides.none? {|side| side <= 0}) &&
-      (sum_one_two > @triangle_sides[2] && sum_one_three > @triangle_sides[1] && sum_two_three > @triangle_sides[0])
-      return true
-    else
-      return false
-    end
+    result = [@a, @b, @c].all? {|side| side > 0}
+    result && (@a+@b > @c) && (@a+@c > @b) && (@b+@c > @a)
   end
 
   def kind
-    if valid?
-      if @triangle_sides.uniq.length == 1
-        return :equilateral
-      elsif @triangle_sides.uniq.length == 2
-        return :isosceles
-      else
-        return :scalene
-      end
-    else
+    if !valid?
       raise TriangleError
+    elsif @a==@b && @b==@c
+      result = :equilateral
+    elsif @a==@b || @a==@c || @b==@c
+      result = :isosceles
+    else
+      result = :scalene
     end
+    result
   end
+
 end
 
 class TriangleError < StandardError
-
 end
